43879d8b7b5c201d19dd81ba36b7cc92fe4fa35c
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-08-07 22:08:50 +0530

    Update LandingPage.jsx

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./LandingPage/Hero";
 import OurServices from "./LandingPage/OurServices";
+import ProjectIdea from "./LandingPage/ProjectIdea";
 
 const LandingPage = () => {
   return (
     <>
       <Hero />
       <OurServices />
+      <ProjectIdea />
     </>
   );
 };
