705c5385ed897676cf80c9c7b58802d75ff9f48f
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-08-11 14:48:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
+import Login from "./Auth/Login";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <LandingPage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+      {pathname !== "/login" && <Footer />}
+      <Toaster />
     </>
   );
 }
